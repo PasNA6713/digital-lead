182769e518837461858f9c27493a2d4788bd7c90
Leopoldic <44893780+Leopoldic@users.noreply.github.com>
2020-11-14 03:50:29 +0300

    Update classify.py

--- python-backend/project/message_getter/model/classify.py
+++ python-backend/project/message_getter/model/classify.py
@@ -1,4 +1,35 @@
 import random
+import os
+import joblib
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+    
+    PER,
+    NamesExtractor,
+    DatesExtractor,
+    MoneyExtractor,
+    AddrExtractor,
+
+    Doc
+)
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+ner_tagger = NewsNERTagger(emb)
+
+names_extractor = NamesExtractor(morph_vocab)
+dates_extractor = DatesExtractor(morph_vocab)
+money_extractor = MoneyExtractor(morph_vocab)
+addr_extractor = AddrExtractor(morph_vocab)
 
 from loguru import logger
 
@@ -24,10 +55,27 @@ def get_danger_level(text: str) -> int:
     return random.choice(list(DANGER.VALUES))
 
 def classify(text: str) -> str:
-    return EVENT.ColdWater
+    path = os.getcwd()
+    classificator = joblib.load(os.getcwd()+'\\models\\finalized_model.sav')
+    vectorizer = joblib.load(os.getcwd()+'\\models\\vectorizer.sav')
+    vector_text = extractor.transform([text])
+    if classificator.predict(vector_text) == 0:
+        return EVENT.Dtp
+    elif classificator.predict(vector_text) == 1:
+        return EVENT.Fire
+    elif classificator.predict(vector_text) == 2:
+        return EVENT.WarmWater
 
 def find_address(text: str) -> str:
-    return None
+    
+    def get_addres(match):
+        adres = ''
+        for i in match.fact.parts:
+                adres += i.type+' '+i.value+' ' 
+        return(adres[:len(adres)-1])
+
+    match = addr_extractor.find(text)
+    return get_addres(match)
 
 def get_file(file):
     logger.debug(file)
