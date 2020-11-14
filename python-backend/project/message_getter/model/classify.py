cbb610bd784a15a869199a07d0e34fd644e5928a
Leopoldic <44893780+Leopoldic@users.noreply.github.com>
2020-11-15 01:30:05 +0300

    model

--- python-backend/project/message_getter/model/classify.py
+++ python-backend/project/message_getter/model/classify.py
@@ -1,6 +1,43 @@
 import random
+import os
+import joblib
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+    
+    PER,
+    NamesExtractor,
+    DatesExtractor,
+    MoneyExtractor,
+    AddrExtractor,
+
+    Doc
+)
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+ner_tagger = NewsNERTagger(emb)
+
+names_extractor = NamesExtractor(morph_vocab)
+dates_extractor = DatesExtractor(morph_vocab)
+money_extractor = MoneyExtractor(morph_vocab)
+addr_extractor = AddrExtractor(morph_vocab)
 
 from loguru import logger
+from classificator import preproccesor
+from clasterizator import main_patrol_func
+
+import sys
+sys.path.append('./models/tensorflow1/models/research/object_detection')
+from Object_detection_image import get_image
 
 
 class EVENT:
@@ -20,14 +57,39 @@ class DANGER:
     VALUES = {LOW, MEDIUM, HIGH}
 
 
-def get_danger_level(text: str) -> int:
-    return random.choice(list(DANGER.VALUES))
+def get_danger_level(objct):
+    claster = main_patrol_func(objct)
+    if claster.danger == 0:
+        return DANGER.LOW
+    elif claster.danger == 1:
+        return DANGER.MEDIUM
+    elif claster.danger == 2:
+        return DANGER.HIGH
 
 def classify(text: str) -> str:
-    return EVENT.ColdWater
+    path = os.getcwd()
+    classificator = joblib.load(os.getcwd()+'\\models\\finalized_model.sav')
+    vectorizer = joblib.load(os.getcwd()+'\\models\\vectorizer.sav')
+    text = preproccesor(text)
+    vector_text = extractor.transform([text])
+    if classificator.predict(vector_text) == 0:
+        return EVENT.Dtp
+    elif classificator.predict(vector_text) == 1:
+        return EVENT.Fire
+    elif classificator.predict(vector_text) == 2:
+        return EVENT.WarmWater
 
 def find_address(text: str) -> str:
-    return None
+    def get_addres(match):
+        adres = ''
+        for i in match.fact.parts:
+                adres += i.type+' '+i.value+' ' 
+        return(adres[:len(adres)-1])
+    match = addr_extractor.find(text)
+    return get_addres(match)
 
 def get_file(file):
-    logger.debug(file)
+    if get_image(file) == 0:
+        return EVENT.Dtp
+    if get_image(file) == 1:
+        return EVENT.Fire
