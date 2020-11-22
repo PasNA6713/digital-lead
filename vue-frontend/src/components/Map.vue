af15decab1a82fecbdbfeacd5cfb860e949cc4b1
Shepard2154 <john-shepard-2154@yandex.ru>
2020-11-22 11:14:12 -0500

    update Map.vue

--- vue-frontend/src/components/Map.vue
+++ vue-frontend/src/components/Map.vue
@@ -6,15 +6,17 @@
             :coords="mapCenter"
             :zoom="10" 
             :use-object-manager="true"
-            @click="onClick"
+            :controls="['zoomControl']"
+            @map-was-initialized="getMapInstance"
         >
-            <ymap-marker 
+            <!-- <ymap-marker 
             :key="marker.id"
             v-for="marker in placemarks" 
             :coords="marker.coords" 
             marker-id="marker.id" 
             hint-content="marker.id"
-            />
+            @click="onClick"
+            /> -->
         </yandex-map>
         <br>
     </div>
@@ -22,12 +24,14 @@
 
 <script>
     import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+    import dataset from '../assets/dataset.json'
 
     export default{
         components: {
             yandexMap, 
             ymapMarker
         },
+
         data: () => ({
             settings: {
                 apiKey: 'e70694c3-ce7f-4459-b7f6-be3d53e2cc8e',
@@ -35,33 +39,67 @@
                 coordorder: 'latlong',
                 version: '2.1'
             },
-            // isObjectManager: true,
             mapCenter: [59.9370, 30.3089],
-            placemarks: 
-                [
-                    {
-                        "id": 1,
-                        "coords": [59.9, 30.55]
-                    },
-                    {
-                        "id": 2,
-                        "coords": [59.945800, 30.271400]
-                    },
-                    {
-                        "id": 3,
-                        "coords": [59.945800, 30.271000]
-                    }
-                ],
+
+            currentMap: null,
+            objectManager: null,
+            placemarks: [],
+
             classifier: {
                 1: 'yellow',
                 2: 'blue',
                 3: 'red'
             }
-            }),
+        }),
+
         methods: {
             onClick(e) {
                 this.placemarks = e.get('coords');
+        },
+            async getMapInstance(map) {
+                if (map) {
+                try {
+                    this.currentMap = map
+                    this.objectManager = new ymaps.ObjectManager({
+                                clusterize: true,
+                                gridSize: 32,
+                                clusterDisableClickZoom: true
+                            })
+                        try {
+                            this.objectManager.add(this.placemarks)
+                            this.currentMap.geoObjects.add(this.objectManager)
+                        } catch (error) {
+                            console.log('no placemarks!')
+                        }
+                        
+                } catch (error) {
+                    console.log(error)
+                }
+            }
         }
+    },
+    mounted(){
+        // Получение json и преобразование в валидные маркеры для карты
+        for (let i=0;i<dataset.length;i++){
+            let mapMarker = {
+                            type: 'Feature',
+                            id: dataset[i]["id"],
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [dataset[i]["address"]["latitude"], dataset[i]["address"]["longtitude"]]
+                            },
+                            properties: {
+                                hintContent: dataset[i]["date"],
+                                balloonContent: dataset[i]["text"]
+                            },
+                            options: {
+                                preset: "islands#dotIcon",
+                                iconColor: this.classifier[dataset[i]["danger_level"]]
+                            }
+            }
+            this.placemarks.push(mapMarker)
+        }
+
     }
 }
 </script>
